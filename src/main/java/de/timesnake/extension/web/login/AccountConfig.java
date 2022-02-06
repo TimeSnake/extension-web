b10e41ed8b796f90fc708dc09d1f6ad1fea46e7f
funzter <cavehive@protonmail.com>
2022-02-06 23:32:44 +0100

    init

---
+++ src/main/java/de/timesnake/extension/web/login/AccountConfig.java
@@ -0,0 +1,68 @@
+package de.timesnake.extension.web.login;
+
+import de.timesnake.basic.proxy.util.file.ExFile;
+
+public class AccountConfig extends ExFile {
+
+    private static final String DATABASE_NAME = "database.name";
+    private static final String DATABASE_URL = "database.url";
+    private static final String DATABASE_USER = "database.user";
+    private static final String DATABASE_PASSWORD = "database.password";
+    private static final String DATABASE_TABLE_NAME = "database.table";
+    private static final String DATABASE_UUID_COLUMN_NAME = "database.column.uuid";
+    private static final String DATABASE_NAME_COLUMN_NAME = "database.column.name";
+    private static final String DATABASE_CODE_COLUMN_NAME = "database.column.code";
+    private static final String DATABASE_DATE_COLUMN_NAME = "database.column.date";
+
+    private static final String ENABLED = "enabled";
+
+    private static final String VERIFCATION_CODE_LENGTH = "verification_code.length";
+
+    public AccountConfig() {
+        super("exweb", "login_config");
+    }
+
+    public String getDatabaseName() {
+        return super.getString(DATABASE_NAME);
+    }
+
+    public String getDatabaseUrl() {
+        return super.getString(DATABASE_URL);
+    }
+
+    public String getDatabaseUser() {
+        return super.getString(DATABASE_USER);
+    }
+
+    public String getDatabasePassword() {
+        return super.getString(DATABASE_PASSWORD);
+    }
+
+    public String getDatabaseTableName() {
+        return super.getString(DATABASE_TABLE_NAME);
+    }
+
+    public String getDatabaseUuidColumnName() {
+        return super.getString(DATABASE_UUID_COLUMN_NAME);
+    }
+
+    public String getDatabaseNameColumnName() {
+        return super.getString(DATABASE_NAME_COLUMN_NAME);
+    }
+
+    public String getDatabaseCodeColumnName() {
+        return super.getString(DATABASE_CODE_COLUMN_NAME);
+    }
+
+    public Integer getVerifcationCodeLength() {
+        return super.getInt(VERIFCATION_CODE_LENGTH);
+    }
+
+    public String getDatabaseDateColumnName() {
+        return super.getString(DATABASE_DATE_COLUMN_NAME);
+    }
+
+    public boolean isEnabled() {
+        return super.getBoolean(ENABLED);
+    }
+}

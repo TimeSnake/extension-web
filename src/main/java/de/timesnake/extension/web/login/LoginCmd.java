80cace0bad46ace61e43773b73517ac89ad3493b
funzter <cavehive@protonmail.com>
2022-02-07 12:13:30 +0100

    fixed cyclic dependency

--- src/main/java/de/timesnake/extension/web/login/LoginCmd.java
+++ src/main/java/de/timesnake/extension/web/login/LoginCmd.java
@@ -1,15 +1,15 @@
 package de.timesnake.extension.web.login;
 
-import de.timesnake.basic.proxy.util.Network;
 import de.timesnake.basic.proxy.util.chat.Argument;
 import de.timesnake.basic.proxy.util.chat.ChatColor;
 import de.timesnake.basic.proxy.util.chat.Sender;
 import de.timesnake.basic.proxy.util.user.User;
 import de.timesnake.extension.web.chat.Plugin;
 import de.timesnake.extension.web.main.ExWeb;
-import de.timesnake.library.basic.util.cmd.Arguments;
-import de.timesnake.library.basic.util.cmd.CommandListener;
-import de.timesnake.library.basic.util.cmd.ExCommand;
+import de.timesnake.library.extension.util.chat.Chat;
+import de.timesnake.library.extension.util.cmd.Arguments;
+import de.timesnake.library.extension.util.cmd.CommandListener;
+import de.timesnake.library.extension.util.cmd.ExCommand;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
@@ -43,13 +43,13 @@ public class LoginCmd implements CommandListener<Sender, Argument> {
                     sender.sendPluginMessage(ChatColor.PERSONAL + "Your old code is now expired");
                 }
                 TextComponent link = new TextComponent();
-                link.setText(Network.getChat().getSenderPlugin(Plugin.WEB) + ChatColor.PERSONAL + ChatColor.UNDERLINE + "Click here" + ChatColor.PERSONAL + " to open your personal login link");
+                link.setText(Chat.getSenderPlugin(Plugin.WEB) + ChatColor.PERSONAL + ChatColor.UNDERLINE + "Click here" + ChatColor.PERSONAL + " to open your personal login link");
                 link.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to open the link, " + ChatColor.WARNING + "do NOT share with others")));
                 link.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url.getUrl()));
                 sender.getPlayer().sendMessage(link);
 
                 TextComponent code = new TextComponent();
-                code.setText(Network.getChat().getSenderPlugin(Plugin.WEB) + ChatColor.PERSONAL + "Code: " + ChatColor.VALUE + url.getCode());
+                code.setText(Chat.getSenderPlugin(Plugin.WEB) + ChatColor.PERSONAL + "Code: " + ChatColor.VALUE + url.getCode());
                 code.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to copy to clipboard, " + ChatColor.WARNING + "do NOT share with others")));
                 code.setClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, url.getCode()));
                 sender.getPlayer().sendMessage(code);

3e19649881654614af22db0422c5b70036be1911
funzter <cavehive@protonmail.com>
2022-07-12 20:49:43 +0200

    update to velocity

--- src/main/java/de/timesnake/extension/web/login/LoginCmd.java
+++ src/main/java/de/timesnake/extension/web/login/LoginCmd.java
@@ -1,19 +1,19 @@
 package de.timesnake.extension.web.login;
 
 import de.timesnake.basic.proxy.util.chat.Argument;
-import de.timesnake.basic.proxy.util.chat.ChatColor;
 import de.timesnake.basic.proxy.util.chat.Sender;
 import de.timesnake.basic.proxy.util.user.User;
 import de.timesnake.extension.web.chat.Plugin;
 import de.timesnake.extension.web.main.ExWeb;
+import de.timesnake.library.basic.util.chat.ChatColor;
 import de.timesnake.library.extension.util.chat.Chat;
 import de.timesnake.library.extension.util.cmd.Arguments;
 import de.timesnake.library.extension.util.cmd.CommandListener;
 import de.timesnake.library.extension.util.cmd.ExCommand;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.chat.hover.content.Text;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
 
 import java.util.List;
 
@@ -35,19 +35,18 @@ public class LoginCmd implements CommandListener<Sender, Argument> {
 
         User user = sender.getUser();
 
-        if ("login".equals(args.getString(0).toLowerCase())) {
+        if ("login".equalsIgnoreCase(args.getString(0))) {
             LoginUrl url = ExWeb.getAccountManager().registerUser(user);
 
             if (url != null) {
                 if (url.expiredOldCode()) {
                     sender.sendPluginMessage(ChatColor.PERSONAL + "Your old link is now expired");
                 }
-                TextComponent link = new TextComponent();
-                link.setText(Chat.getSenderPlugin(Plugin.WEB) + ChatColor.PERSONAL + ChatColor.UNDERLINE + "Click " +
-                        "here" + ChatColor.PERSONAL + " to open your personal login link");
-                link.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                        new Text("Click to open the link, " + ChatColor.WARNING + "do NOT share with others")));
-                link.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url.getUrl()));
+                TextComponent link = Component.text(Chat.getSenderPlugin(Plugin.WEB) + ChatColor.PERSONAL +
+                        ChatColor.UNDERLINE + "Click here" + ChatColor.PERSONAL + " to open your personal login link");
+                link.hoverEvent(HoverEvent.hoverEvent(HoverEvent.Action.SHOW_TEXT,
+                        Component.text("Click to open the link, " + ChatColor.WARNING + "do NOT share with others")));
+                link.clickEvent(ClickEvent.clickEvent(ClickEvent.Action.OPEN_URL, url.getUrl()));
                 sender.getPlayer().sendMessage(link);
             } else {
                 sender.sendPluginMessage(ChatColor.WARNING + "Error during login. Please contact an admin!");

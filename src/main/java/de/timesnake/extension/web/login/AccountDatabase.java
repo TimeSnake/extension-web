05446d08f8c29999dae4bf0c108e4e6d2b6da6a2
funzter <funzter@mail.timesnake.de>
2022-12-26 18:11:39 +0100

    Updated copyright

--- src/main/java/de/timesnake/extension/web/login/AccountDatabase.java
+++ src/main/java/de/timesnake/extension/web/login/AccountDatabase.java
@@ -1,27 +1,17 @@
 /*
- * extension-web.main
  * Copyright (C) 2022 timesnake
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; If not, see <http://www.gnu.org/licenses/>.
  */
 
 package de.timesnake.extension.web.login;
 
 import de.timesnake.basic.proxy.util.Network;
 import de.timesnake.extension.web.chat.Plugin;
-
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLNonTransientConnectionException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;

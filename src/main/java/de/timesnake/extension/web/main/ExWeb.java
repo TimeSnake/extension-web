f010071fcddc92f9f451b9cc5bc8bd61253d17c6
funzter <cavehive@protonmail.com>
2022-12-26 18:11:39 +0100

    Updated copyright

--- src/main/java/de/timesnake/extension/web/main/ExWeb.java
+++ src/main/java/de/timesnake/extension/web/main/ExWeb.java
@@ -1,19 +1,5 @@
 /*
- * extension-web.main
  * Copyright (C) 2022 timesnake
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; If not, see <http://www.gnu.org/licenses/>.
  */
 
 package de.timesnake.extension.web.main;
@@ -27,7 +13,6 @@ import com.velocitypowered.api.proxy.ProxyServer;
 import de.timesnake.basic.proxy.util.Network;
 import de.timesnake.extension.web.login.AccountManager;
 import de.timesnake.extension.web.login.LoginCmd;
-
 import java.util.logging.Logger;
 
 @Plugin(id = "extension-web", name = "ExWeb", version = "1.0-SNAPSHOT",

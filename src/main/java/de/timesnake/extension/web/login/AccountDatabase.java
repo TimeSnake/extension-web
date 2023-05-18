dc118513085a58afb87942295057d6a5d9dcca6b
funzter <funzter@mail.timesnake.de>
2023-05-18 11:27:34 +0200

    reindented files with two spaces

--- src/main/java/de/timesnake/extension/web/login/AccountDatabase.java
+++ src/main/java/de/timesnake/extension/web/login/AccountDatabase.java
@@ -18,167 +18,167 @@ import java.util.UUID;
 
 public class AccountDatabase {
 
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    protected final String tableName;
-    protected final String uuidColumnName;
-    protected final String nameColumnName;
-    protected final String codeColumnName;
-    private final String user;
-    private final String password;
-    protected String name;
-    protected Connection connection;
-    protected String url;
+  private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+  protected final String tableName;
+  protected final String uuidColumnName;
+  protected final String nameColumnName;
+  protected final String codeColumnName;
+  private final String user;
+  private final String password;
+  protected String name;
+  protected Connection connection;
+  protected String url;
 
-    public AccountDatabase(String name, String url, String user, String password, String tableName,
-            String uuidColumnName, String nameColumnName, String codeColumnName) {
-        this.url = url;
-        this.user = user;
-        this.password = password;
-        this.name = name;
-        this.tableName = tableName;
-        this.uuidColumnName = uuidColumnName;
-        this.nameColumnName = nameColumnName;
-        this.codeColumnName = codeColumnName;
-        Network.runTaskAsync(this::connect);
+  public AccountDatabase(String name, String url, String user, String password, String tableName,
+      String uuidColumnName, String nameColumnName, String codeColumnName) {
+    this.url = url;
+    this.user = user;
+    this.password = password;
+    this.name = name;
+    this.tableName = tableName;
+    this.uuidColumnName = uuidColumnName;
+    this.nameColumnName = nameColumnName;
+    this.codeColumnName = codeColumnName;
+    Network.runTaskAsync(this::connect);
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public String getUser() {
+    return user;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void connect() {
+    try {
+      Class.forName("org.mariadb.jdbc.Driver");
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
     }
 
-    public String getUrl() {
-        return url;
+    try {
+      DriverManager.registerDriver(new org.mariadb.jdbc.Driver());
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
     }
 
-    public String getUser() {
-        return user;
+    try {
+      connection = DriverManager.getConnection(this.getUrl(), this.getUser(),
+          this.getPassword());
+    } catch (SQLNonTransientConnectionException e) {
+      Network.printWarning(Plugin.WEB, "Can not connect to web login database");
+      e.printStackTrace();
+      return;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return;
     }
 
-    public String getPassword() {
-        return password;
+    Statement ps = null;
+    try {
+      ps = connection.createStatement();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
 
-    public void connect() {
-        try {
-            Class.forName("org.mariadb.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            DriverManager.registerDriver(new org.mariadb.jdbc.Driver());
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-        try {
-            connection = DriverManager.getConnection(this.getUrl(), this.getUser(),
-                    this.getPassword());
-        } catch (SQLNonTransientConnectionException e) {
-            Network.printWarning(Plugin.WEB, "Can not connect to web login database");
-            e.printStackTrace();
-            return;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        Statement ps = null;
-        try {
-            ps = connection.createStatement();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            ps.executeUpdate(
-                    """
-                            CREATE FUNCTION IF NOT EXISTS BIN_TO_UUID(bin BINARY(16))
-                            RETURNS CHAR(36) DETERMINISTIC
-                            BEGIN
-                              DECLARE hex CHAR(32);
-                              SET hex = HEX(bin);
-                              RETURN LOWER(CONCAT(LEFT(hex, 8), '-', MID(hex, 9, 4), '-', MID(hex, 13, 4), '-', MID(hex, 17, 4), '-', RIGHT(hex, 12)));
-                            END;
-                            """);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            ps.executeUpdate(
-                    """
-                            CREATE FUNCTION IF NOT EXISTS UUID_TO_BIN(uuid CHAR(36))
-                            RETURNS BINARY(16) DETERMINISTIC
-                            BEGIN
-                              RETURN UNHEX(CONCAT(REPLACE(uuid, '-', '')));
-                            END;
-                            """);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        Network.printText(Plugin.WEB, "Connected to web database");
+    try {
+      ps.executeUpdate(
+          """
+              CREATE FUNCTION IF NOT EXISTS BIN_TO_UUID(bin BINARY(16))
+              RETURNS CHAR(36) DETERMINISTIC
+              BEGIN
+                DECLARE hex CHAR(32);
+                SET hex = HEX(bin);
+                RETURN LOWER(CONCAT(LEFT(hex, 8), '-', MID(hex, 9, 4), '-', MID(hex, 13, 4), '-', MID(hex, 17, 4), '-', RIGHT(hex, 12)));
+              END;
+              """);
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
 
-    public void disconnect() {
-        try {
-            if (connection != null) {
-                connection.close();
-            }
-        } catch (SQLException ignored) {
-        }
+    try {
+      ps.executeUpdate(
+          """
+              CREATE FUNCTION IF NOT EXISTS UUID_TO_BIN(uuid CHAR(36))
+              RETURNS BINARY(16) DETERMINISTIC
+              BEGIN
+                RETURN UNHEX(CONCAT(REPLACE(uuid, '-', '')));
+              END;
+              """);
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
 
-    public Connection getConnection() {
-        return connection;
+    Network.printText(Plugin.WEB, "Connected to web database");
+  }
+
+  public void disconnect() {
+    try {
+      if (connection != null) {
+        connection.close();
+      }
+    } catch (SQLException ignored) {
+    }
+  }
+
+  public Connection getConnection() {
+    return connection;
+  }
+
+  public boolean isConnected() {
+    try {
+      return this.connection != null && !this.connection.isClosed();
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
+    }
+    return false;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public boolean addEntry(UUID uuid, String name, String code) {
+    try {
+      PreparedStatement ps =
+          this.connection.prepareStatement(
+              "INSERT INTO " + this.tableName + " (" + this.uuidColumnName +
+                  ", " + this.nameColumnName + ", " + this.codeColumnName + ")" +
+                  "VALUES (UUID_TO_BIN(\"" + uuid.toString() + "\"), \"" + name
+                  + "\", \"" + code + "\")"
+                  + "ON DUPLICATE KEY UPDATE "
+                  + this.codeColumnName + "= \"" + code + "\";");
+      ps.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return false;
     }
 
-    public boolean isConnected() {
-        try {
-            return this.connection != null && !this.connection.isClosed();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return false;
-    }
+    return true;
+  }
 
-    public String getName() {
-        return name;
+  public boolean isEntryExisting(UUID uuid) {
+    try {
+      ResultSet rs =
+          this.connection.prepareStatement(
+                  "SELECT BIN_TO_UUID(" + this.uuidColumnName + ") AS "
+                      + this.uuidColumnName
+                      + " FROM " + this.tableName
+                      + " WHERE " + this.uuidColumnName + " = \"" + uuid.toString()
+                      + "\";")
+              .executeQuery();
+      if (rs.next()) {
+        return rs.getString(this.uuidColumnName) != null;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
-
-    public boolean addEntry(UUID uuid, String name, String code) {
-        try {
-            PreparedStatement ps =
-                    this.connection.prepareStatement(
-                            "INSERT INTO " + this.tableName + " (" + this.uuidColumnName +
-                                    ", " + this.nameColumnName + ", " + this.codeColumnName + ")" +
-                                    "VALUES (UUID_TO_BIN(\"" + uuid.toString() + "\"), \"" + name
-                                    + "\", \"" + code + "\")"
-                                    + "ON DUPLICATE KEY UPDATE "
-                                    + this.codeColumnName + "= \"" + code + "\";");
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        return true;
-    }
-
-    public boolean isEntryExisting(UUID uuid) {
-        try {
-            ResultSet rs =
-                    this.connection.prepareStatement(
-                                    "SELECT BIN_TO_UUID(" + this.uuidColumnName + ") AS "
-                                            + this.uuidColumnName
-                                            + " FROM " + this.tableName
-                                            + " WHERE " + this.uuidColumnName + " = \"" + uuid.toString()
-                                            + "\";")
-                            .executeQuery();
-            if (rs.next()) {
-                return rs.getString(this.uuidColumnName) != null;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
+    return false;
+  }
 
 }
--- src/main/java/de/timesnake/extension/web/main/ExWeb.java
+++ src/main/java/de/timesnake/extension/web/main/ExWeb.java
@@ -16,38 +16,38 @@ import de.timesnake.extension.web.login.LoginCmd;
 import java.util.logging.Logger;
 
 @Plugin(id = "extension-web", name = "ExWeb", version = "1.0-SNAPSHOT",
-        url = "https://git.timesnake.de", authors = {"MarkusNils"},
-        dependencies = {
-                @Dependency(id = "basic-proxy")
-        })
+    url = "https://git.timesnake.de", authors = {"MarkusNils"},
+    dependencies = {
+        @Dependency(id = "basic-proxy")
+    })
 public class ExWeb {
 
-    public static AccountManager getAccountManager() {
-        return accountManager;
-    }
+  public static AccountManager getAccountManager() {
+    return accountManager;
+  }
 
-    public static ExWeb getPlugin() {
-        return plugin;
-    }
+  public static ExWeb getPlugin() {
+    return plugin;
+  }
 
-    private static ExWeb plugin;
-    private static AccountManager accountManager;
-    private static ProxyServer server;
-    private static Logger logger;
+  private static ExWeb plugin;
+  private static AccountManager accountManager;
+  private static ProxyServer server;
+  private static Logger logger;
 
-    @Inject
-    public ExWeb(ProxyServer server, Logger logger) {
-        ExWeb.server = server;
-        ExWeb.logger = logger;
-        plugin = this;
-    }
+  @Inject
+  public ExWeb(ProxyServer server, Logger logger) {
+    ExWeb.server = server;
+    ExWeb.logger = logger;
+    plugin = this;
+  }
 
-    @Subscribe
-    public void onProxyInitialization(ProxyInitializeEvent event) {
-        accountManager = new AccountManager();
+  @Subscribe
+  public void onProxyInitialization(ProxyInitializeEvent event) {
+    accountManager = new AccountManager();
 
-        Network.getCommandManager().addCommand(this, "web", new LoginCmd(),
-                de.timesnake.extension.web.chat.Plugin.WEB);
+    Network.getCommandManager().addCommand(this, "web", new LoginCmd(),
+        de.timesnake.extension.web.chat.Plugin.WEB);
 
-    }
+  }
 }

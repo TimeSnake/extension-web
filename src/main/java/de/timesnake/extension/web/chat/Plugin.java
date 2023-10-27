adc9eae6f98dfa8072cbea467d7b4e72a60b410b
funzter <funzter@mail.timesnake.de>
2023-10-27 23:39:46 +0200

    cleaned up

--- src/main/java/de/timesnake/extension/web/chat/Plugin.java
+++ src/main/java/de/timesnake/extension/web/chat/Plugin.java
@@ -4,15 +4,11 @@
 
 package de.timesnake.extension.web.chat;
 
-import de.timesnake.library.basic.util.LogHelper;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Plugin extends de.timesnake.basic.proxy.util.chat.Plugin {
 
-  public static final Plugin WEB = new Plugin("Web", "XWB", LogHelper.getLogger("Web", Level.INFO));
+  public static final Plugin WEB = new Plugin("Web", "XWB");
 
-  protected Plugin(String name, String code, Logger logger) {
-    super(name, code, logger);
+  protected Plugin(String name, String code) {
+    super(name, code);
   }
 }
